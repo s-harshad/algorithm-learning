30ae6f52823ea914dbea12032b09d09e3aee20f9
Harshad Shrishrimal <harshadkumar.s@tavant.com>
2017-11-28 20:53:51 -0500

    Implementation of a Bag using arrays.

---
+++ src/main/java/self/learning/algos/bag/ResizingArrayBag.java
@@ -0,0 +1,116 @@
+package self.learning.algos.bag;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ *  The {@code ResizingArrayBag} class represents a bag (or multiset) of generic items 
+ *  It supports insertion and iterating over the items in arbitrary order
+ *  <p>
+ *  This implementation uses a resizing array
+ *  The <em>add</em> operation takes constant amortized time
+ *  The <em>isEmpty</em>, and <em>size</em> operations take constant time
+ *  Iteration takes time proportional to the number of items
+ *
+ *  @author Harshad Shrishrimal
+ *  @param <Item> 
+ */
+public class ResizingArrayBag<Item> implements Iterable<Item> {
+    
+    private Item[] a; // Array to hold a collection of items
+    private int numberOfElements; // number of elements on bag
+    
+    /**
+     * Create a bag with initial capacity of 2
+     * When the capacity is full, it will be resized to double it's capacity
+     */
+    public ResizingArrayBag() {
+        a = (Item[]) new Object[2]; // at first we'll make space for 2 items
+        numberOfElements = 0; // explictly set size to 0
+    }
+    
+    /**
+     * Create a bag with initial capacity specified by user
+     * When the capacity is full, it will be resized to double it's capacity
+     * @param initialCapacity initialCapacity for the number of items
+     */
+    public ResizingArrayBag(int initialCapacity) {
+        a = (Item[]) new Object[initialCapacity]; // initialize array
+        numberOfElements = 0; // explictly set size to 0
+    }
+    
+    /**
+     * Is this bag empty ?
+     * @return true if empty; false otherwise
+     */
+    public boolean isEmpty() {
+        return numberOfElements == 0;
+    }
+    
+    /**
+     * Return the number of items in this bag
+     * @return the number of items in this bag
+     */
+    public int size() {
+        return numberOfElements;
+    }
+    
+    /**
+     * Adds the item to this bag.
+     * @param item the item to be added to this bag.
+     */
+    public void add(Item item) {
+        
+        // double the size of the array if necessary
+        if(a.length == numberOfElements) {
+            resize(2*numberOfElements);
+        }
+        
+        // add the item in the array
+        a[numberOfElements++] = item;
+    }
+    
+    /**
+     * Returns an iterator that iterates over the items in the bag, arbitrary order
+     * @return an iterator that iterates over the items in the bag, arbitrary order
+     */
+    @Override
+    public Iterator<Item> iterator() {
+        return new ArrayIterator();
+    }
+
+    private void resize(int newCapacity) {
+        // new array with increased capacity
+        Item[] b = (Item[]) new Object[newCapacity];
+        
+        // copy from existing array to new array
+        for(int i=0; i<numberOfElements; i++) {
+            b[i] = a[i];
+        }
+        
+        // point existing to new array
+        a = b;
+    }
+
+    private class ArrayIterator implements Iterator<Item> {
+
+        private int currentElementIndex = 0;
+        
+        @Override
+        public boolean hasNext() {
+            return currentElementIndex < numberOfElements;
+        }
+
+        @Override
+        public Item next() {
+            if(!hasNext()) { throw new NoSuchElementException(); }
+            return a[currentElementIndex++];
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+    
+}
---
+++ src/test/java/self/learning/algos/bag/ResizingArrayBagTest.java
@@ -0,0 +1,61 @@
+package self.learning.algos.bag;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+/** 
+ * 
+ * @author Harshad Shrishrimal
+ */
+@RunWith(JUnitPlatform.class)
+public class ResizingArrayBagTest {
+
+    @Test
+    @DisplayName("Bag should be empty upon creation")
+    public void bagEmptyUponCreation() throws Exception {
+        ResizingArrayBag<String> bagOfString = new ResizingArrayBag<>();
+        Assert.assertTrue(bagOfString.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Should be able to create a bag with initial capacity")
+    public void bagCreationWithUserSepcifiedInitialCapacity() {
+        
+        ResizingArrayBag<String> bagOfString = new ResizingArrayBag<>(3);
+        bagOfString.add("Item1");
+        bagOfString.add("Item2");
+        bagOfString.add("Item3");
+        
+        List<String> actuals = new ArrayList<>();
+        for (String s : bagOfString) {
+            actuals.add(s);
+        }
+
+        Assert.assertThat(actuals, Matchers.containsInAnyOrder("Item1", "Item2", "Item3"));
+    }
+    
+    @Test
+    @DisplayName("Should be able to retrieve items from bag")
+    public void retrieveElementsFromBag_1() throws Exception {
+
+        ResizingArrayBag<String> bagOfString = new ResizingArrayBag<>();
+        bagOfString.add("Item1");
+        bagOfString.add("Item2");
+        bagOfString.add("Item3");
+
+        List<String> actuals = new ArrayList<>();
+        for (String s : bagOfString) {
+            actuals.add(s);
+        }
+
+        Assert.assertThat(actuals, Matchers.containsInAnyOrder("Item1", "Item2", "Item3"));
+    }
+    
+    
+}
