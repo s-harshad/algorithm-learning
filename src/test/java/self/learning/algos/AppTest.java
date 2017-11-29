48c6f4c85a13395ac175c751d8b96eef49aed8de
Harshad Shrishrimal <harshadkumar.s@tavant.com>
2017-11-28 19:34:03 -0500

    first commit

---
+++ src/test/java/self/learning/algos/AppTest.java
@@ -0,0 +1,18 @@
+package self.learning.algos;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author shrishrimal
+ */
+@RunWith(JUnitPlatform.class)
+public class AppTest {
+    
+    @Test    
+    public void test1() throws Exception {
+        System.out.println("Hello Test");
+    }
+}
